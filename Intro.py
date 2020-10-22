abc4d6bf7162cdd52a4363c4bb0666064ddbb418
Aditya Deshpande <72786936+deshpande-aditya@users.noreply.github.com>
2020-10-22 11:34:07 +0530

    Added some intro codes

---
+++ Intro.py
@@ -0,0 +1,9 @@
+
+# printing first code for me!
+print("Hello World!!")
+
+#check some other functions :P
+char_name='Aditya'
+print(char_name + " is a cool guy")
+char_name1=char_name.upper()
+print(char_name1.isupper())
